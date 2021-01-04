3807bce0019335bc421ac9fc779d77bd17cc71f8
Sivert Randen <sivert.randen@dnb.no>
2021-01-04 12:06:37 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'dnb-ui-lib/style';
+import './App.scss';
+import { H1 } from 'dnb-ui-lib';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <H1>Todo app</H1>
     </div>
   );
 }
---
+++ src/dnb-ui-lib.d.ts
@@ -0,0 +1,1 @@
+declare module 'dnb-ui-lib/style';
